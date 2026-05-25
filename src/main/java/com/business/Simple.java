320147118778651cca36c061d6ff18ef4d1c5ef2
fulvios <fulviocaruna@gmail.com>
2026-05-25 08:22:28 +0200

    second commit

--- src/main/java/com/business/Simple.java
+++ src/main/java/com/business/Simple.java
@@ -5,7 +5,7 @@ public class Simple {
 	public static void main(String[] args) throws InterruptedException {
 
 		while (true) {
-			System.out.println("Version 1");
+			System.out.println("Version 1a");
 			Thread.sleep(10000);
 		}
 
