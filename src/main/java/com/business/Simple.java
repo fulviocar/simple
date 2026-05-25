26ad7103cefd3bf0e66533f0dbcce0fb4b7c92bd
fulvios <fulviocaruna@gmail.com>
2026-05-25 08:11:38 +0200

    commit 1

---
+++ src/main/java/com/business/Simple.java
@@ -0,0 +1,14 @@
+package com.business;
+
+public class Simple {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		while (true) {
+			System.out.println("Version 1");
+			Thread.sleep(10000);
+		}
+
+	}
+
+}
