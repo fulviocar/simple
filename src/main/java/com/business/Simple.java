bfd2cc8515876286feed5efa46f7801bda284d39
fulvios <fulviocaruna@gmail.com>
2026-05-25 19:13:47 +0200

    commit100

--- src/main/java/com/business/Simple.java
+++ src/main/java/com/business/Simple.java
@@ -5,7 +5,7 @@ public class Simple {
 	public static void main(String[] args) throws InterruptedException {
 
 		while (true) {
-			System.out.println("Version 2a");
+			System.out.println("Version 2.0.2");
 			Thread.sleep(10000);
 		}
 
